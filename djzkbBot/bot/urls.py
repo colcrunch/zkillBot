79ed0cf5553240278603801b09ea324c2e30ab8f
colcrunch <rhartnett35@gmail.com>
2022-12-23 16:08:07 +0000

    Update url references to use app names.

--- djzkbBot/bot/urls.py
+++ djzkbBot/bot/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "bot"
+
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('css_test/', views.css_test, name='css_test'),
