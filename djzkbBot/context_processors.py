bf693f4eb67ea135e817a01a3378ba83f4d3a77d
colcrunch <rhartnett35@gmail.com>
2023-01-01 01:47:15 +0000

    Dynamically build discord login URL

--- djzkbBot/context_processors.py
+++ djzkbBot/context_processors.py
@@ -1,7 +1,16 @@
+import urllib.parse
+
 from django.conf import settings
 
 
 def djzkb_settings(request):
+    d_encoded = urllib.parse.quote(settings.DISCORD_CALLBACK_URL, safe='')
+    d_redirect = (f"https://discord.com/api/oauth2/authorize"
+                  f"?client_id={settings.DISCORD_APP_ID}"
+                  f"&redirect_uri={d_encoded}"
+                  f"&response_type=code"
+                  f"&scope={'%20'.join(settings.DISCORD_LOGIN_TOKEN_SCOPES)}")
     return {
         'SITE_NAME': settings.SITE_NAME,
+        'DISCORD_LOGIN_URL': d_redirect,
     }
