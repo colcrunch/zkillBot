9a67c7a9ef5f4e65a693d0bbf0527595d6109dea
colcrunch <rhartnett35@gmail.com>
2023-01-22 23:36:04 +0000

    Add discord_user_id to UserProfile

--- djzkbBot/authentication/models.py
+++ djzkbBot/authentication/models.py
@@ -10,6 +10,12 @@ class UserProfile(models.Model):
         related_name='profile',
         on_delete=models.CASCADE
     )
+    discord_user_id = models.CharField(
+        max_length=255,
+        db_index=True,
+        null=True,
+        help_text="The discord user id for which this token is valid."
+    )
 
     class Meta:
         default_permissions = (())
