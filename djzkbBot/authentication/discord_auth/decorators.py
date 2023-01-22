99bf53c79d948aebd83cd4b74e31396981f87400
colcrunch <rhartnett35@gmail.com>
2023-01-22 21:31:02 +0000

    Implement the discord_token_required decorator and the discord login flow. (still needs authentication backend)

--- djzkbBot/authentication/discord_auth/decorators.py
+++ djzkbBot/authentication/discord_auth/decorators.py
@@ -1,9 +1,26 @@
 from logging import getLogger
 from functools import wraps
 
-from .models import Token
+from .models import Token, DiscordCallbackRedirect
+from .views import sso_redirect
 
 logger = getLogger(__name__)
+
+
+def _check_callback(request):
+    if not request.session_exists(request.session.session_key):
+        logger.debug(f"Creating session for {request.user}")
+        request.session.create()
+
+    try:
+        model = DiscordCallbackRedirect.objects.get(session_key=request.session.session_key)
+        token = Token.objects.get(pk=model.token.pk)
+        model.delete()
+        logger.debug(f"Retrieved new token from callback for {request.user} session {request.session.session_key [:5]}")
+        return token
+    except:
+        logger.debug(f"No callback for {request.user} session {request.session.session_key [:5]}", exc_info=True)
+        return None
 
 
 def discord_token_required(scopes='', new=False):
@@ -18,7 +35,15 @@ def discord_token_required(scopes='', new=False):
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
-            return
+            # Check if we are coming back from SSO with a token.
+            token = _check_callback(request)
+            if token:
+                logger.debug("Got new token from {request.user} session {request.session.session_key [:5]}. Returning.")
+                return view_func(request, token, *args, **kwargs)
+
+            # otherwise send user to SSO to add a new token
+            logger.debug("Redirecting {request.user} session {request.session.session_key [:5]} to SSO.")
+            return sso_redirect(request, scopes=scopes)
         return
 
     return decorator
--- djzkbBot/authentication/discord_auth/views.py
+++ djzkbBot/authentication/discord_auth/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.conf import settings
+from requests_oauthlib import OAuth2Session
 
-from .models import Token
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.conf import settings
+from django.urls import reverse
+
+from .models import Token, DiscordCallbackRedirect
+from .decorators import discord_token_required
 
 from logging import getLogger
 
@@ -10,16 +16,94 @@ logger = getLogger(__name__)
 
 
 # Create your views here.
+@discord_token_required(scopes=settings.DISCORD_LOGIN_SCOPES)
 def login(request, token):
-    pass
+    user = authenticate(token=token)
+    if user:
+        token.user = user
+        if Token.objects.exclude(pk=token.pk).equivalent_to(token).require_valid().exists():
+            token.delete()
+        else:
+            token.save()
+
+        login(request, user)
+        return redirect(request.POST.get('next', request.GET.get('next', settings.LOGIN_REDIRECT_URL)))
+    messages.error(request, "Unable to authenticate using the selected token.")
+    return redirect(settings.LOGIN_URL)
 
 
 def callback(request):
-    code = request.GET.get('code')
+    """
+    Parses the SSO callback, validates, and retrieves Token.
+    Also, internally redirects to target URL.
+    """
+    logger.debug(f"Recieved callback for {request.user} session {request.session.session_key[:5]}")
 
-    token = Token.objects.create_from_code(code)
+    code = request.GET.get('code', None)
+    state = request.GET.get('state', None)
+    if not code or not state:
+        logger.warning("Missing parameters for code exchange.")
+        return HttpResponseBadRequest()
 
-    return HttpResponse(str(token.__dict__))
+    callback = get_object_or_404(
+        DiscordCallbackRedirect,
+        state=state,
+        session_key=request.session.session_key
+    )
+    token = Token.objects.create_from_request(request)
+    callback.token = token
+    callback.save()
+
+    logger.debug(
+        f"Processed callback for {request.user}" 
+        f"session {request.session.session_key[:5]}. Redirecting to {callback.url}"
+    )
+
+    return redirect(callback.url)
+
+
+def sso_redirect(request, scopes=None, return_to=None):
+    """
+    Generates a DiscordCallbackRedirect for the specified request.
+    Redirects to Discord for login.
+    Accepts a view or url name as a redirect after SSO.
+    """
+    logger.debug(
+        f"Initiating redirect of {request.user}" 
+        f"session {request.session.session_key[:5] if request.session.session_key else '[no key]'}"
+    )
+    if scopes is None:
+        scopes = list()
+    elif isinstance(scopes, str):
+        scopes = list([scopes])
+
+    # ensure only one redirect object per session
+    if request.session.session_key:
+        DiscordCallbackRedirect.objects.filter(session_key=request.session.session_key).delete()
+
+    # ensure there is a session
+    if not request.session.exists(request.session.session_key):
+        logger.debug("Creating new session.")
+        request.session.create()
+
+    if return_to:
+        url = reverse(return_to)
+    else:
+        url = request.get_full_path()
+
+    oauth = OAuth2Session(
+        settings.DISCORD_APP_ID,
+        redirect_uri=settings.DISCORD_CALLBACK_URL,
+        scopes=scopes
+    )
+    redirect_url, state = oauth.authorization_url(settings.DISCORD_OAUTH_LOGIN_URL)
+
+    DiscordCallbackRedirect.objects.create(
+        session_key=request.session.session_key,
+        state=state,
+        url=url
+    )
+    return redirect(redirect_url)
 
 
 def revoke_token(request):
