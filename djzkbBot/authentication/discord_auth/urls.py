3087e3172fd87a22fc2851c48f9de16700f586bb
colcrunch <rhartnett35@gmail.com>
2023-01-23 01:37:34 +0000

    Update url namespaces

--- djzkbBot/authentication/discord_auth/urls.py
+++ djzkbBot/authentication/discord_auth/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = "dauth"
+app_name = "discord_auth"
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.login, name='discord_login'),
     path('callback/', views.callback, name='discord_callback'),
     path('token/revoke/', views.revoke_token, name='revoke_token'),
     path('token/refresh/', views.refresh_token, name='refresh_token'),
--- djzkbBot/authentication/urls.py
+++ djzkbBot/authentication/urls.py
@@ -8,5 +8,5 @@ app_name = "authentication"
 urlpatterns = [
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
-    path('discord/', include(urls))
+    path('discord/', include((urls, 'discord_auth'), namespace='discord_auth'))
 ]
