5337b56d3c16c2f52c30d7c1adfcf4794e2c9a32
colcrunch <rhartnett35@gmail.com>
2023-03-07 11:25:06 +0000

    Require the user to be superuser to access the admin dashboard.

--- djzkbBot/bot/views.py
+++ djzkbBot/bot/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+
 
 # Create your views here.
+def is_superuser(user):
+    return user.is_superuser
+
 @login_required
 def dashboard(request):
     return render(request, 'bot/dashboard.html')
 
 
 @login_required
+@user_passes_test(is_superuser)
 def admin(request):
     return render(request, 'bot/admin.html')
 
